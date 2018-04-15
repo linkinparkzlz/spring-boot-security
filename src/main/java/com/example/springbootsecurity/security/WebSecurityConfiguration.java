b6a16e5aa96360032afcd109793a781c631c878d
zoulvzhou <940933073@qq.com>
2018-04-15 23:27:28 +0800

    CSPF 实例

---
+++ src/main/java/com/example/springbootsecurity/security/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.springbootsecurity.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class WebSecurityConfiguration   extends WebSecurityConfigurerAdapter{
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().csrfTokenRepository(new CookieCsrfTokenRepository()).requireCsrfProtectionMatcher(
+                new RequestMatcher() {
+                    @Override
+                    public boolean matches(HttpServletRequest httpServletRequest) {
+                        return httpServletRequest.getMethod().equals("POST");
+                        //&& httpServletRequest.getRequestURI().startsWith("/login");
+                    }
+                }
+        );
+    }
+}
